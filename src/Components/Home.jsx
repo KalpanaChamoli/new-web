5fea2ca74c810821dfa132f8a225c812f3e8d77e
kalpanaqurilo <kalpana@qurilo.com>
2025-07-14 10:28:54 +0530

    updated code

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,14 +10,14 @@ function Home() {
     return (
         <>
             <div className="relative w-full h-[80vh]">
-                {/* Background Image */}
+
                 <img
                     src="/src/assets/new Image.png"
                     alt="heroImage"
                     className="w-full h-full object-cover absolute top-0 left-0 z-0"
                 />
 
-                {/* Dark Overlay + Text */}
+
                 <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-center  bg-opacity-60 text-white px-6 z-10">
                     <h1 className="text-3xl md:text-5xl font-bold mb-4">
                         Building Intelligent Digital Solutions — End-to-End
@@ -29,29 +29,128 @@ function Home() {
                     </p>
                 </div>
             </div>
-
+            {/* our service */}
             <div className='flex flex-col items-center mt-10'>
                 <h1 className='text-4xl font-bold '>Our Services</h1>
                 <p className='text-2xl mt-4'> End-to-end development tailored to your business needs</p>
-                </div>
+            </div>
 
-                <div className='flex flex-wrap justify-center items-center px-4 py-4  mt-5 gap-4'>
-                    {cards.map((card,index)=>(
+            <div className='flex flex-wrap justify-center items-center px-4 py-4  mt-5 gap-4'>
+                {cards.map((card, index) => (
 
-                        <div key={index} className="flex flex-col items-center p-4 m-2 bg-[#e8bad1] h-[300px] w-[300px] rounded-lg shadow-lg ">
-                            <img src={card.icon} alt={card.title} className="w-16 h-16 mb-2 mt-2" />
-                            <h2 className="text-xl font-semibold px-4 py-4">{card.title}</h2>
-                            <p className="text-gray-600 px-4 py-4 text-center font-medium">{card.description}</p>
+                    <div key={index} className="flex flex-col items-center p-4 m-2 bg-[#e8bad1] h-[300px] w-[300px] rounded-lg shadow-lg ">
+                        <img src={card.icon} alt={card.title} className="w-16 h-16 mb-2 mt-2" />
+                        <h2 className="text-xl font-semibold px-4 py-4">{card.title}</h2>
+                        <p className="text-gray-600 px-4 py-4 text-center font-medium">{card.description}</p>
 
+                    </div>
+                ))}
+            </div>
+            {/* Technology Section */}
+            <div className='bg-[#FDF1EB]'>
+
+                <div className='text-center text-4xl font-bold px-5 py-5 mt-10'>Technology</div>
+                <div className='flex flex-wrap gap-4 items-center mt-10  px-30 py-20'>
+
+                    <div className=' border border-black h-[400px] w-[400px] rounded-2xl hover:shadow-lg bg-white transition-shadow duration-300 hover:bg-black hover:text-white'>
+                        <h1 className='text-center text-2xl font-bold mt-6 '>backend</h1>
+
+                        <div className='flex flex-wrap gap-6 px-8 py-8'>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
 
-                <div>
-                    <div>Technology</div>
-                  
-                   
+
+                    <div className='border border-black h-[500px] w-[400px] rounded-2xl hover:shadow-lg transition-shadow duration-300 hover:bg-black hover:text-white bg-white'>
+                        <h1 className='text-center text-2xl font-bold mt-6 '>Frontend devlopment</h1>
+
+                        <div className='flex flex-wrap gap-2 px-8 py-8 '>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='border border-black h-[400px] w-[400px] rounded-2xl hover:shadow-lg transition-shadow duration-300 hover:bg-black hover:text-white bg-white'>
+                        <h1 className='text-center text-2xl font-bold mt-6'>Dveops Devlopment</h1>
+
+                        <div className='flex flex-wrap gap-2 px-8 py-8'>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                            <div>
+                                <img src='src/assets/node-js.png' alt='node js' className='h-[60px]' />
+                                <h1>Node.js</h1>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
         </>
     )
 }
